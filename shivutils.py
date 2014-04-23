e98d29dc51f021e48edef8ad623947de79e57fd7
Isaac Shivvers <ishivvers@berkeley.edu>
2014-04-22 20:52:19 -0700

    updated bias correction; NOTE: the kast_bias.pro script accounts for different gains off of the different amplifiers; if this is important I will replicate

--- shivutils.py
+++ shivutils.py
@@ -6,7 +6,18 @@ Author: Isaac Shivvers, ishivvers@berkeley.edu
 
 Steals heavily from Brad Cenko's kast_redux and iqutils packages.
 (thanks Brad!)
+
+
+Notes:
+ - the scipy.signal.argrelmin function may be helpful to find
+    minima in the standard star spectra, afterwhich you can fit
+    with spectools.
+ - e.g.: mins = scipy.signal.argrelmin( fl, order=25 )
 """
+
+######################################################################
+# imports
+######################################################################
 
 import numpy as np
 import pyfits as pf
@@ -16,121 +27,86 @@ from pyraf import iraf
 from types import ListType, StringType
 
 ######################################################################
-# global variables, kast-specific
+# global variables
 ######################################################################
+
+# kast parameters
 REDBIAS='[1201:1231,*]'
-REDTRIM='[1:1200,11:170]'
+REDTRIM='[1:1200,%d:%d]'
 REDGAIN=3.0
 REDRDNOISE=12.5
 BLUEBIAS1='[2052:2080,*]'
 BLUEBIAS2='[2082:2110,*]'
-BLUETRIM1='[1:1024,31:300]'
-BLUETRIM2='[1025:2048,31:300]'
-BLUEGAIN=1.2
+BLUETRIM1='[1:1024,%d:%d]'
+BLUETRIM2='[1025:2048,%d:%d]'
+BLUEGAIN1=1.2
+BLUEGAIN1=1.237
 BLUERDNOISE=3.7
+
+# location of line id list
 COORDLIST='./caldir/licklinelist.dat'
 
+# useful for iraf interactions
+yes=iraf.yes
+no=iraf.no
+INDEF=iraf.INDEF
+
 ######################################################################
-# file and os management
+# fits file management
 ######################################################################
 
-def update_head(inlist,inkeys,invals,comments="",extn=0):
-    """
-     update header keywords using pyfits
-    """
-    
-    # Images to process
-    if type(inlist) is ListType:
-        infiles=inlist
-    elif type(inlist) is StringType:
-        infiles=iraffiles(inlist)
-    else:
-        raise StandardError( "Please pass a string or list of input files" )
+def head_get( image, keywords ):
+    '''
+    Get given keywords from the given fits image.
+    '''
+    if type(keywords) != list:
+        keywords = [keywords]
+    hdu = pf.open(image)
+    values = []
+    for key in keywords:
+        values.append( hdu[0].header[key] )
+    hdu.close()
+    return values
 
-    # Input checking
-    if type(inkeys)==ListType and type(invals)!=ListType:
-        raise StandardError( "Keywords and values must both be lists" )
+############################################################################
 
-    # Header keywords to update
-    if type(inkeys) is StringType:
-        keys=[inkeys]
-        vals=[invals]
-        cmts=[comments]
-    elif type(inkeys) is ListType:
-        keys=inkeys
-        vals=invals
-        if type(comments) is ListType:
-            cmts=comments
-        else:
-            cmts=None
-    else:
-        raise StandardError( "Please pass a string or list of header keywords to update" )
+def head_del( images, keywords ):
+    '''
+    Remove given keywords from fits images.
+    '''
+    if type(images) != list:
+        images = [images]
+    if type(keywords) != list:
+        keywords = [keywords]
+    for image in images:
+        hdu = pf.open(image)
+        for key in keywords:
+            hdu[0].remove(key)
+        hdu.writeto( image, clobber=True )
+        hdu.close()
 
-    # Loop over files
-    nkeys=len(keys)
-    for file in infiles:
+############################################################################
 
-        check_exist(file,"r")
-        inf=pyfits.open(file,"update")
+def head_update( images, keywords, values, comment=None ):
+    '''
+    Update (or set) given keywords to given values for every image
+     in given images.
+    If comment is included, will add that comment to every updated keyword.
+    ''' 
+    if type(images) != list:
+        images = [images]
+    if type(keywords) != list:
+        keywords = [keywords]
+    if type(values) != list:
+        values = [values]
+    for image in images:
+        hdu = pf.open(image)
+        for i,key in enumerate(keywords):
+            hdu[0].header.set(key, values[i], comment)
+        hdu.writeto( image, clobber=True )
+        hdu.close()
 
-        if extn>=len(inf):
-            inf.close()
-            raise StandardError( "Requested extension [%d] does not exist" % extn )
-
-        for i in xrange(nkeys):
-            if cmts:
-                inf[extn].header.update(keys[i],vals[i],comment=cmts[i])
-            else:
-                inf[extn].header.update(keys[i],vals[i])
-
-        inf.close()
-
-
-def get_head(file,keys,extn=0,verbose=globver):
-
-    """ reads one or more header keywords from a FITS file
-        using PyFITS """
-
-    vals=[]
-    check_exist(file,"r")
-
-    try:
-        fimg=pyfits.open(file)
-        if extn>=len(fimg):
-            if verbose:
-                print "Requested extension [%d] does not exist" % extn
-            vals=[""]*len(keys)
-            return vals
-
-        head=fimg[extn].header
-
-        if type(keys)==StringType:
-            key=keys
-            if head.has_key(key):
-                vals=head[key]
-            else:
-                if verbose:
-                    print "Error reading keyword %s from %s" % (key,file)
-                vals=""
-        elif type(keys)==ListType:
-            for key in keys:
-                if head.has_key(key):
-                    vals.append(head[key])
-                else:
-                    if verbose:
-                        print "Error reading keyword %s from %s" % (key,file)
-                    vals.append("")
-        else:
-            if verbose:
-                print "Bad variable type for keys"
-        fimg.close()
-
-    except:
-        print "Error reading header of %s" % file
-        vals=[""]*len(keys)
-
-    return vals
-    
+############################################################################
 
 def run_cmd( cmd, ignore_errors=False ):
     """
@@ -139,12 +115,68 @@ def run_cmd( cmd, ignore_errors=False ):
     res = system( cmd )
     if not ignore_errors:
         if res != 0:
-            raise StandardError( "Error ::: command failed :::" )
-
+            raise StandardError( "Error ::: command failed ::: "+cmd )
 
 ############################################################################
 # bias and flatfielding
 ############################################################################
+
+def bias_correct(images, y1, y2, prefix=None):
+    '''
+    Bias correct a set of images with y trim limits of y1, y2.
+    NOTE: the original kast_bias.pro script incorporates the "gain" of
+          each amplifier independently, but it looks like ccdproc does
+          not do this.  If important, will need to hardcode own version
+          of kast_bias.pro.
+    '''
+    if type(images) != list:
+        images = [images]
+    if prefix == None:
+        outputs = ["" for i in images]
+    else:
+        outputs = [prefix+i for i in images]
+    for i,image in enumerate(images):
+        # get whether this is red or blue side from the fits header
+        side = head_get( image, 'VERSION' )
+
+        if prefix == None:
+            outname = image
+        else:
+            outname = outputs[i]
+        
+        if side == 'kastr':
+            # use the red side parameters
+            iraf.ccdproc(image, output=outputs[i], ccdtype='', noproc=no, fixpix=no,
+                         overscan=yes,trim=yes, zerocor=no, darkcor=no, flatcor=no,
+                         illumcor=no, fringecor=no, readcor=no, scancor=no,
+                         biassec=REDBIAS, trimsec=REDTRIM%(y1,y2))
+        
+        elif side == 'kastb':
+            # need to process the two different blue amplifiers seperately
+            root,ext=path.splitext(image)
+            iraf.ccdproc(image, output='%s_1'%root, ccdtype='', noproc=no, fixpix=no,
+                         overscan=yes, trim=yes, zerocor=no, darkcor=no, flatcor=no,
+                         illumcor=no, fringecor=no, readcor=no, scancor=no,
+                         biassec=BLUEBIAS1, trimsec=BLUETRIM1%(y1,y2))
+            iraf.ccdproc(image, output='%s_2'%root, ccdtype='', noproc=no, fixpix=no,
+                         overscan=yes, trim=yes, zerocor=no, darkcor=no, flatcor=no,
+                         illumcor=no, fringecor=no, readcor=no, scancor=no,
+                         biassec=BLUEBIAS2, trimsec=BLUETRIM2%(y1,y2))
+            # add the results back together and clean the directory
+            iraf.imjoin('%s_1,%s_2' % (root, root), 'j%s' % image, 1)
+            run_cmd( 'rm %s_1 %s_2' % (root, root) )
+            
+            # need to manually move combined file
+            run_cmd( 'mv j%s %s' % (image, outname) )
+            
+            # not sure if needed, but update the header info to reflect the new array size
+            head_update(outname, ['CCDSEC', 'DATASEC'],
+                        ['[1:2048,1:%d]'%(y2-y1+1), '[1:2048,1:%d]'%(y2-y1+1)])
+        
+        # add the dispersion axis keyword, saying the x axis is the wavelength axis
+        head_update(outname, 'DISPAXIS', 1 )
+    
+############################################################################    
 
 def redbias(images, prefix=None, biassec=REDBIAS, trimsec=REDTRIM):
     '''
