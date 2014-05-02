2a519001fbbb345f25d46aa339cc4dcc2f3e5d8d
isaac <ishivvers@berkeley.edu>
2014-05-02 16:05:32 -0700

    Added extensive logging capability to kastshiv (untested)

--- kastshiv.py
+++ kastshiv.py
@@ -16,7 +16,7 @@ Notes:
 
 import shivutils as su
 import os
-from glob import glob
+import logging
 
 class Shiv(object):
     """
@@ -25,11 +25,27 @@ class Shiv(object):
      pipeline and the B.Cenko pipeline - thanks everyone).
     """
     
-    def __init__(self, runID=runID, interactive=True):
+    def __init__(self, runID, interactive=True, logfile=None):
         self.runID = runID
         self.interactive = True  #STILL NEEDS TO BE IMPLEMENTED
+
+        # set up the logfile
+        if logfile == None:
+            logfile = self.runID+'.reduction.log'
+        self.log = logger.getLogger('TheKastShiv')
+        # have log messages go to screen and to file
+        fh = logging.FileHandler( logfile )
+        fh.setLevel(logging.INFO)
+        fh.setFormatter( logging.Formatter('%(asctime)s - %(levelname)s - %(message)s') )
+        self.log.addHandler(fh)
+        sh = logging.StreamHandler()
+        sh.setLevel(logging.INFO)  # could adjust this to adjust the verbosity level
+        sh.setFormatter( logging.Formatter('%(levelname)s - %(message)s') )
+        self.log.addHandler(sh)
+
         self.steps = [self.build_fs,
                       self.get_data,
+                      self.define_lists,
                       self.find_trim_sections,
                       self.trim_and_bias_correct,
                       self.update_headers,
@@ -47,11 +63,15 @@ class Shiv(object):
         return self
     
     def next(self):
-        self.steps[ self.current_step ]()
-        self.current_step += 1
+        if self.current_step < len(self.steps):
+            self.steps[ self.current_step ]()
+            self.current_step += 1
+        else:
+            raise StopIteration
 
     def build_fs(self):
         """Create the file system hierarchy and enter it."""
+        self.log.info('Creating file system for run %s'%self.runID)
         su.make_file_system( self.runID )
         os.chdir( self.runID )
 
@@ -60,12 +80,19 @@ class Shiv(object):
         Download data and populate relevant folders.
         Should be run from root folder, and leaves you in working folder.
         """
+        self.log.info('Downloading data for %s'%datePT)
         os.chdir( 'rawdata' )
         su.get_kast_data( datePT )
         os.chdir( '../working' )
         self.objects, self.flats, self.arcs = su.wiki2elog( datestring=dateUT, runID=self.runID, outfile='%s.log'%self.runID  )
         su.populate_working_dir( 'uc', logfile='%s.log'%self.runID )
 
+    def define_lists(self):
+        """
+        Parses the output of wiki2elog into the formats needed here;
+         must have self.objects, self.flats, self.arcs defined (output of wiki2elog).
+        """
+        self.log.info('Populating file lists from log')
         # define the file lists
         self.robjects = [o for o in self.objects if o[1]==2]
         self.bobjects = [o for o in self.objects if o[1]==1]
@@ -88,15 +115,18 @@ class Shiv(object):
         """
         Determine the optimal trim sections for each side.
         """
+        self.log.info('Fitting for optimal trim sections')
         # find the trim sections for the red and blue images,
         #  using the first red and blue flats
         self.b_ytrim = su.find_trim_sec( self.apf+self.broot%self.barcs[0][0] )
         self.r_ytrim = su.find_trim_sec( self.apf+self.rroot%self.rarcs[0][0] )
+        self.log.info( '\nBlue trim section: %s \nRed trim section: %s'%(str(self.b_ytrim), str(self.r_ytrim)) )
 
     def trim_and_bias_correct(self):
         """
         Trims and bias corrects images.
         """
+        self.log.info('Trimming and bias correcting all images')
         assert(self.opf == self.fpf == self.apf)
         blues = [self.opf+self.broot%o[0] for o in self.bobjects+self.bflats+self.barcs]
         su.bias_correct_idl( blues, self.b_ytrim[0], self.b_ytrim[1] )
@@ -105,12 +135,15 @@ class Shiv(object):
         su.bias_correct_idl( reds, self.r_ytrim[0], self.r_ytrim[1] )
 
         self.opf = self.fpf = self.apf = 'b'+self.opf
+        self.log.info( 'Applied trim section %s to following files:\n'+',\n'.join(blues)%str(self.b_ytrim) )
+        self.log.info( 'Applied trim section %s to following files:\n'+',\n'.join(reds)%str(self.r_ytrim) )
 
     def update_headers(self):
         """
         Inserts the airmass and optimal PA into the headers for
          each image, along with other header fixes.
         """
+        self.log.info('Updating headers of all images')
         assert(self.opf == self.fpf == self.apf)
         files = [self.opf+self.broot%o[0] for o in self.bobjects+self.bflats+self.barcs] +\
                 [self.opf+self.rroot%o[0] for o in self.robjects+self.rflats+self.rarcs]
@@ -120,8 +153,10 @@ class Shiv(object):
         """
         Makes combined and normalized flats for each side.
         """
+        self.log.info('Creating flats')
         blues = [self.fpf+self.broot%o[0] for o in self.bflats]
         su.make_flat( blues, 'nflat1', 'blue' )
+        self.log.info( 'Created flat nflat1 out of the following files:\n'+',\n'.join(blues) )
 
         # go through and make the combined and normalized red flats for each object
         allgroups = set([o[2] for o in self.objects])
@@ -130,14 +165,18 @@ class Shiv(object):
             reds = [self.fpf+self.rroot%o[0] for o in self.rflats if o[2]==i]
             if len(redflats) != 0:
                 su.make_flat( reds, 'nflat%d'%i, 'red' )
+                self.log.info( 'Created flat nflat%d out of the following files:\n'%i+',\n'.join(reds) )
+
 
     def apply_flats(self):
         """
         Applies flatfields to each side.
         """
+        self.log.info('Applying flatfield correction to all images')
         assert(self.opf == self.apf)
         blues = [self.opf+self.broot%o[0] for o in self.bobjects+self.barcs]
         su.apply_flat( blues, 'nflat1' )
+        self.log.info( 'Applied flat nflat1 to the following files:\n'+',\n'.join(blues) )
 
         # go through and apply the correct flat for each object
         allgroups = set([o[2] for o in self.objects])
@@ -146,6 +185,7 @@ class Shiv(object):
             reds = [self.opf+self.rroot%o[0] for o in self.robjects+self.rarcs if o[2]==i]
             if len(reds) != 0:
                 su.apply_flat( reds, 'nflat%d'%i )
+                self.log.info( 'Applied flat nflat%d to the following files:\n'%i+',\n'.join(reds) )
 
         self.opf = self.apf = 'f'+self.opf
 
@@ -153,12 +193,15 @@ class Shiv(object):
         """
         Performs cosmic ray rejection on all objects.
         """
+        self.log.info("Perfoming cosmic ray removal")
         blues = [self.opf+self.broot%o[0] for o in self.bobjects]
         for b in blues:
             su.clean_cosmics( b, 'blue' )
-        blues = [self.opf+self.rroot%o[0] for o in self.robjects]
+        self.log.info( 'Removed cosmic rays from the folloing files:\n'+',\n'.join(blues) )
+        reds = [self.opf+self.rroot%o[0] for o in self.robjects]
         for r in reds:
             su.clean_cosmics( r, 'red' )
+        self.log.info( 'Removed cosmic rays from the folloing files:\n'+',\n'.join(reds) )
 
         self.opf = 'c'+self.opf
 
@@ -166,6 +209,7 @@ class Shiv(object):
         """
         Extracts the spectra from each object.
         """
+        self.log.info('Extracting spectra for red objects')
         # extract all red objects on the first pass
         extracted_objects = []  #used to keep track of multiple observations of the same object
         extracted_images = []
@@ -175,10 +219,11 @@ class Shiv(object):
             #  as a reference.
             try:
                 reference = extracted_images[ extracted_objects.index( o[4] ) ]
-                print '\n\nusing',reference,'for reference on',o[4]
                 su.extract( fname, 'red', reference=reference )
+                self.log.info('Used ' + reference,' for reference on '+ fname +' (object: '+o[4]+')')
             except ValueError:
                 su.extract( fname, 'red' )
+                self.log.info('Extracted '+fname)
             extracted_objects.append( o[4] )
             extracted_images.append( fname )
         # extract all blue objects on the second pass
@@ -188,15 +233,16 @@ class Shiv(object):
             #  as a reference or apfile reference (accounting for differences in blue and red pixel scales).
             try:
                 reference = extracted_images[ extracted_objects.index( o[4] ) ]
-                print '\n\nusing',reference,'for reference on',o[4],'\n'
                 if 'blue' in reference:
                     # go ahead and simply use as a reference
                     su.extract( fname, 'blue', reference=reference )
+                    self.log.info('Used ' + reference,' for reference on '+ fname +' (object: '+o[4]+')')
                 elif 'red' in reference:
                     # Need to pass along apfile and conversion factor to map the red extraction
                     #  onto this blue image. Blue CCD has a plate scale 1.8558 times larger than the red.
                     apfile = 'database/ap'+reference.strip('.fits')
                     su.extract( fname, 'blue', apfile=apfile, apfact=1.8558)
+                    self.log.info('Used apfiles from ' + reference,' for reference on '+ fname +' (object: '+o[4]+')')
                 else:
                     raise StandardError( 'We have a situation with aperature referencing.' )
             except ValueError:
@@ -208,11 +254,13 @@ class Shiv(object):
         """
         Extracts the spectra from each arc.
         """
+        self.log.info('Extracting arc spectra')
         # extract the blue arc from the beginning of the night using the first blue
         #  object as the reference
         bluearc = self.apf+self.broot%(self.barcs[0][0])
         refblue = self.opf+self.broot%(self.bobjects[0][0])
         su.extract( bluearc, 'blue', arc=True, reference=refblue )
+        self.log.info('Extracted blue arc spectrum '+bluearc+' using '+refblue+' as a reference')
 
         # extract the red arcs, using each associated object as a reference
         allgroups = set([o[2] for o in self.objects])
@@ -222,24 +270,30 @@ class Shiv(object):
             refred = [self.opf+self.rroot%o[0] for o in objects if o[2]==i][0]
             for redarc in redarcs:
                 su.extract( redarc, 'red', arc=True, reference=refred )
+                self.log.info('Extracted red arc spectrum '+redarc+' using '+refred+' as a reference')
 
     def id_arcs(self):
         """
         Go through and identify and fit for the lines in all arc files.
         """
+        self.log.info("Identifying arc lines and fitting for wavelength solutions")
         # ID the blue side arc
         bluearc = self.apf+self.ebroot%(self.barcs[0][0])
         su.id_arc( bluearc )
+        self.log.info("Successfully ID'd "+bluearc)
 
         # sum the R1 and R2 red arcs from the beginning of the night and id the result
         R1R2 = [self.apf+self.erroot%o[0] for o in arcs][:2]
         su.combine_arcs( R1R2, 'Combined_0.5_Arc.ms.fits' )
+        self.log.info("Created Combined_0.5_Arc.ms.fits from "+str(R1R2))
         su.id_arc( 'Combined_0.5_Arc.ms.fits' )
+        self.log.info("Successfully ID'd Combined_0.5_Arc.ms.fits" )
 
         # ID the first red object arc interactively, making sure
         #  we handle the 0.5" arcs properly
         firstobjarc = [self.apf+self.erroot%o[0] for o in arcs][2]
         su.reid_arc( firstobjarc, 'Combined_0.5_Arc.ms.fits')
+        self.log.info("ID'd "+firstobjarc+" using Combined_0.5_Arc.ms.fits as a reference")
 
         # now go through all other red arcs automatically
         allgroups = set([o[2] for o in self.objects])
@@ -248,14 +302,17 @@ class Shiv(object):
         for i in allgroups:
             objarc = [self.apf+self.erroot%o[0] for o in self.rarcs if o[2]==i][0]
             su.reid_arc( objarc, firstobjarc, interact=False )
+            self.log.info("ID'd "+objarc+" using "+firstobjarc+" as a reference")
 
     def apply_wavelength(self):
         """
         Apply the relevant wavelength solution to each object.
         """
+        self.log.info("Appying wavelength solution to all objects")
         bluearc = self.apf+self.ebroot%(self.barcs[0][0])
         for o in self.bobjects:
             su.disp_correct( self.opf+self.ebroot%o[0], bluearc )
+            self.log.info("Applied wavelength solution from "+bluearc+" to "+self.opf+self.ebroot%o[0])
 
         for o in self.robjects:
             # first red object includes the beginning arcs; account for that
@@ -265,15 +322,25 @@ class Shiv(object):
             else:
                 redarc = redarcs[0]
             su.disp_correct( self.opf+self.erroot%o[0], redarc )
+            self.log.info("Applied wavelength solution from "+redarc+" to "+self.opf+self.erroot%o[0])
 
     def flux_calibrate(self):
         """
         Determine and apply the relevant flux calibration to all objects.
         """
+        self.log.info("Flux calibrating all objects")
         # match objects to standards take at the closest airmass
         allobjects = [self.opf+self.ebroot%o[0] for o in self.bobjects] +\
                      [self.opf+self.erroot%o[0] for o in self.robjects]
         blue_std_dict, red_std_dict = su.match_science_and_standards( allobjects )
+
+        tmp = blue_std_dict.copy()
+        tmp.update(red_std_dict)
+        for k in tmp.keys():
+            self.log.info( "Associated the following files with standard "+k+":\n"+',\n'.join(tmp[k]) )
+
         # apply flux calibrations
         su.calibrate_idl( blue_std_dict )
+        self.log.info( "Applied flux calibrations to blue objects" )
         su.calibrate_idl( red_std_dict )
+        self.log.info( "Applied flux calibrations to red objects" )
